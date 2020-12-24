1f88bab4a5ef6c1f8b4118700c536710f893c8fe
rohan <rhnshiva@gmail.com>
2020-12-24 13:14:21 -0600

    Add colors

--- client/lib/util/Config.dart
+++ client/lib/util/Config.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/material.dart';
+
 class Config{
 
   static final String fontFamily = "Inter";
+  static final Color bgColor = Color(0xff86F4E1);
+  static final Color secondaryColor = Color(0xff3355FF);
  
 }
